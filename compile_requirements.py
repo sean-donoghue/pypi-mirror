dcf1909c8e59ea55875e46c3ff7d58b47713c92c
Sean Donoghue <sean.donoghue@pm.me>
2024-02-25 15:33:00 +0000

    feat: use asyncio for pip-compile script
    Signed-off-by: Sean Donoghue <sean.donoghue@pm.me>

--- compile_requirements.py
+++ compile_requirements.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import subprocess
 
@@ -8,18 +9,30 @@ REQUIREMENTS_PATH = os.path.join(os.path.dirname(__file__), "requirements")
 PIP_COMPILE_ARGS = ["--strip-extras"]
 
 
-def main() -> None:
-    processes = []
+async def main() -> None:
+    print(f"Looking for requirements.in files in path: {REQUIREMENTS_PATH}")
 
+    coros = []
     for requirements_file in os.listdir(REQUIREMENTS_PATH):
         if requirements_file.endswith(".in"):
+            print(f"Found requirements file: {requirements_file}")
             input_path = os.path.join(REQUIREMENTS_PATH, requirements_file)
-            p = subprocess.Popen(["pip-compile", input_path, *PIP_COMPILE_ARGS])
-            processes.append(p)
+            coro = pip_compile_async(input_path)
+            coros.append(coro)
 
-    for p in processes:
-        p.wait()
+    print(f"Running pip-compile on {len(coros)} requirements files")
+    await asyncio.gather(*coros)
+
+    print("All requirements files have been compiled")
+
+
+async def pip_compile_async(input_path: str) -> None:
+    cmd = ["pip-compile", input_path, *PIP_COMPILE_ARGS]
+    process = await asyncio.create_subprocess_exec(*cmd, stderr=subprocess.DEVNULL)
+    await process.wait()
+
+    print(f"pip-compile finished for file: {input_path}")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
